7844456dc224172d031e4e6d8a6c04f1efad8122
cabboose <shayanftw@gmail.com>
2025-07-05 21:17:37 +0800

    init

---
+++ Program.fs
@@ -0,0 +1,49 @@
+﻿open Fake.Core
+open Fake.IO
+open Farmer
+open Farmer.Builders
+
+open Helpers
+
+initializeContext ()
+
+let basePath = __SOURCE_DIRECTORY__
+let clientPath = basePath + @"\Partas.Solid.Start.Template"
+
+Target.create "Clean" (fun _ ->
+    run fable [
+        "clean"
+        "-e"
+        ".fs.jsx"
+        "--yes"
+    ] clientPath
+    )
+
+Target.create "Dev" (fun _ ->
+    [
+        "vinxi", npm [ "run";"dev" ] basePath
+        "fable", fable [ "watch"; "-e";".fs.jsx";"--optimize"
+                         // Compatibility with Partas.Solid.FablePlugin
+                         "-c";"Release"
+                         // Interop compatibility with most JS libraries
+                         "--typedArrays";"false"
+                          ] clientPath
+    ] |> runParallel
+    )
+
+Target.create "Restore" (fun _ ->
+    [
+        "npm", npm [ "install" ] clientPath
+        "dotnet", dotnet [ "restore"; "Partas.Solid.Start.Template.sln" ] basePath
+    ] |> runParallel
+    )
+
+open Fake.Core.TargetOperators
+
+let dependencies = [
+    "Clean" ==> "Dev"
+    "Restore"
+]
+
+[<EntryPoint>]
+let main args = runOrDefault args
